c2dea14e11e894c884b90f437b41f777eb630def
Crysmon <mr.crysmon@mail.ru>
2022-11-29 22:09:41 +0300

    New example added

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        System.out.println("Hello bstrd!");
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the random number: ");
+
+        int a = in.nextInt();
+
+        while (a % 2 != 0) {
+            System.out.printf("Число %d нечётное \n", a);
+            a = a / 2;
+            System.out.printf("Теперь а = %d! \n", a);
+        }
+
+        System.out.printf("Число %d чётное \n", a);
+        System.out.printf("Your new number: %d \n", a);
+        in.close();
+        }
 }
